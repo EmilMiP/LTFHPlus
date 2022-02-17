8edfe7b8a853fa05cbf78b9aa35062d9f1abdf3d
JetteS <jette.steinbach@post.au.dk>
2022-02-17 13:12:43 +0100

    Remove spelling mistake

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rtmvnorm_gibbs_cpp <- function(P, sd, lower, upper, fixed, to_return, x, n_sim, burn_in) {
-    .Call(`_LTFHplus_rtmvnorm_gibbs_cpp`, P, sd, lower, upper, fixed, to_return, x, n_sim, burn_in)
+    .Call(`_LTFHPlus_rtmvnorm_gibbs_cpp`, P, sd, lower, upper, fixed, to_return, x, n_sim, burn_in)
 }
 
