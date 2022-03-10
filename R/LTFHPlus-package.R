b1be171a9c470faef377252ab9e46188bef0a905
JetteS <jette.steinbach@post.au.dk>
2022-03-10 13:12:42 +0100

    Changing the name LTFHpp back to LTFHPlus

--- R/LTFHPlus-package.R
+++ R/LTFHPlus-package.R
@@ -4,7 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @useDynLib LTFHpp, .registration = TRUE
+#' @useDynLib LTFHPlus, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
