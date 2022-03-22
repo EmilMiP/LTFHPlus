857d4508df133dbdeee1d1509a4551f7f3fcd734
JetteS <jette.steinbach@post.au.dk>
2022-03-22 18:47:18 +0100

    Changing the internal functions to allow for vectors

--- R/Internal_functions.R
+++ R/Internal_functions.R
@@ -1,10 +1,10 @@
-#' Checking that a relative is represented by a valid string
+#' Checking that relatives are represented by valid strings
 #'
-#' \code{check_valid_relatives} checks whether a relative is represented
-#' by a valid abbreviation.
+#' \code{check_valid_relatives} checks whether relatives are represented
+#' by valid abbreviations.
 #'
-#' This function can be used to check whether a relative is represented
-#' by a valid abbreviation. A valid abbreviation is one of the following:
+#' This function can be used to check whether relatives are represented
+#' by valid abbreviations. A valid abbreviation is one of the following:
 #' - g (Genetic component of full liability)
 #' - o (Full liability)
 #' - m (Mother)
@@ -19,8 +19,9 @@
 #' - mau\[0-9\]* (Aunts/Uncles - maternal side)
 #' - pau\[0-9\]* (Aunts/Uncles - paternal side).
 #'
-#' @param relative A string representing a relative.
-#' The string must be one of the following
+#' @param relatives A string or character vector representing 
+#' the relatives.
+#' All strings must be chosen among the following abbreviations
 #' - g (Genetic component of full liability)
 #' - o (Full liability)
 #' - m (Mother)
@@ -36,16 +37,16 @@
 #' - pau\[0-9\]* (Aunts/Uncles - paternal side)
 #' for the function to return TRUE.
 #' 
-#' @return If \code{relative} is a string chosen from the mentioned list of strings,
+#' @return If \code{relatives} is a string or character vector such that
+#' all strings are chosen from the mentioned list of strings,
 #' then the function will return TRUE. Otherwise, the function is aborted.
 #' 
 #' @examples
+#' \dontrun{
 #' check_valid_relatives("g")
 #' check_valid_relatives("o")
 #' check_valid_relatives("mgm")
-#'
-#'
-#' \dontrun{
+#' 
 #' # This will result in errors:
 #' get_relatedness("a")
 #' get_relatedness(m)
@@ -53,15 +54,17 @@
 #' 
 #' @importFrom stringr str_detect
 #' 
-check_valid_relatives <- function(relative){
+check_valid_relatives <- function(relatives){
   
-  if(class(relative) != "character"){
-    stop(paste0(deparse(substitute(relative)), " must be a string!"))
-  }else if(!(str_detect(relative, "^[gomf]$") | str_detect(relative, "^[mp]g[mf]$") | 
-       str_detect(relative, "^s[0-9]*") | str_detect(relative, "^[mp]hs[0-9]*")| 
-       str_detect(relative, "^[mp]au[0-9]*"))){
+  if(class(relatives) != "character"){
+    
+    stop(paste0(deparse(substitute(relatives)), " must be a string or character vector!"))
+  
+  }else if(any(!(str_detect(relatives, "^[gomf]$") | str_detect(relatives, "^[mp]g[mf]$") | 
+       str_detect(relatives, "^s[0-9]*") | str_detect(relatives, "^[mp]hs[0-9]*")| 
+       str_detect(relatives, "^[mp]au[0-9]*")))){
        
-       stop(paste0(deparse(substitute(relative)), " is not a valid string! Use a string from the following list: \n
+       stop(paste0(deparse(substitute(relatives)), " contains invalid abbreviations! Use strings from the following list: \n
   - g (Genetic component of full liability)\n
   - o (Full liability)\n
   - m (Mother)\n
@@ -80,29 +83,28 @@ check_valid_relatives <- function(relative){
   }
 }
 
-#' Checking that a proportion is valid
+#' Checking that proportions are valid
 #'
-#' \code{check_proportion} checks whether a proportion is valid, i.e.
-#' whether it is non-negative and at most one
+#' \code{check_proportion} checks whether proportions are valid, i.e.
+#' whether they are non-negative and at most one.
 #'
-#' This function can be used to check whether a proportion is non-negative
-#' and at most one
+#' This function can be used to check whether proportions are non-negative
+#' and at most one.
 #'
-#' @param prop A number or integer representing the proportion that 
-#' needs to be validated.
+#' @param prop A number, integer or numeric vector representing the proportions that 
+#' need to be validated.
 #' 
-#' @return If \code{prop} is a valid proportion that is of class \code{numeric}
+#' @return If \code{prop} are valid proportions of class \code{numeric}
 #' or \code{integer}, non-negative and at most one,
 #' then the function will return TRUE. Otherwise, the function aborts.
 #' 
 #' @examples
+#' \dontrun{
 #' check_proportion(0.2)
 #' check_proportion(0.04)
 #' check_proportion(0)
 #' check_proportion(1)
-#'
-#'
-#' \dontrun{
+#' 
 #' # This will result in errors:
 #' check_proportion(2)
 #' check_proportion(-0.5)
@@ -111,13 +113,21 @@ check_valid_relatives <- function(relative){
 check_proportion <- function(prop){
   
   if(is.null(prop)){
+    
     stop(paste0(deparse(substitute(prop)), " must be specified!"))
+  
   }else if(class(prop)!= "numeric" && class(prop)!= "integer"){
+    
     stop(paste0(deparse(substitute(prop)), " must be numeric!"))
-  }else if(prop<0){
+  
+  }else if(any(prop<0)){
+    
     stop(paste0(deparse(substitute(prop)), " must be non-negative!"))
-  }else if(prop>1){
+  
+  }else if(any(prop>1)){
+    
     stop(paste0(deparse(substitute(prop)), " must be smaller than or equal to 1!"))
+  
   }else{
     return(TRUE)
   }
@@ -145,11 +155,10 @@ check_proportion <- function(prop){
 #' then the function will return TRUE. Otherwise, the function will be aborted.
 #' 
 #' @examples
+#' \dontrun{
 #' check_correlation_matrix(matrix(c(1,0.4,0.4,1), nrow = 2))
 #' check_correlation_matrix(diag(3))
-#'
-#'
-#' \dontrun{
+#' 
 #' # This will result in errors:
 #' check_correlation_matrix(matrix(c(0.2,0.4,0.4,0.2), nrow = 2))
 #' check_correlation_matrix(matrix(nrow=2, ncol = 2))
@@ -158,9 +167,13 @@ check_proportion <- function(prop){
 check_correlation_matrix <- function(corrmat){
   
   if(is.null(corrmat)){
+    
     stop(paste0(deparse(substitute(corrmat)), " must be specified!"))
+  
   }else if(any(diag(corrmat)!= 1)){
+    
     stop(paste0("All diagonal entries in ", deparse(substitute(corrmat))," must be 1!"))
+  
   }else if(any(abs(corrmat)>1)){
     stop(paste0("All off-diagonal entries in ", deparse(substitute(corrmat))," must be between -1 and 1!"))
   }else if(!isSymmetric.matrix(corrmat)){
