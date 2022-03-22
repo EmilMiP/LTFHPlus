651207e00939b85f794f54986a65d5c1323890f3
EmilMiP <emp@profibermail.dk>
2022-03-22 11:50:08 +0100

    not important

--- R/est_gen_liab_noFH_corr_trait.R
+++ R/est_gen_liab_noFH_corr_trait.R
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 
 #'
 #' Estimate genetic liability similar to LT-FH
@@ -11,6 +12,18 @@
 #'
 #' @return Returns the estimated genetic liabilities.
 #'
+=======
+#' Estimate genetic liability when multiple traits and prevalence information for each trait are available, but family history is not
+#'
+#' @param h2_vec Liability scale heritability of the trait being analysed.
+#' @param gen_cor_vec vector of genetic correlations, in order of appearance in phenotype file
+#' @param phen tibble or data.frame with status of the individual in order of h2_vec.
+#' @param prev_vec Vector of prevalences in order of appearance in h2_vec.
+#' @param tol Convergence criteria of the Gibbs sampler. Default is 0.01, meaning a standard error of the mean below 0.01
+#'
+#' @return Returns the estimated genetic liabilities.
+#'
+>>>>>>> parent of 02fc740... Changing and renaming the function estimate_gen_liability_noFH_corr_traits
 #' @examples 
 #' # See R/Example/example_nosib.R for an example of use and input.
 #' @importFrom dplyr %>%
@@ -30,6 +43,7 @@ estimate_gen_liability_noFH_corr_traits = function(phen,
     dplyr::group_by_all() %>% 
     dplyr::slice_head() %>%
     dplyr::ungroup()
+<<<<<<< HEAD
   
   reduced$post_gen_no_fam <- NA
   reduced$post_gen_no_fam_se <- NA
@@ -47,14 +61,39 @@ estimate_gen_liability_noFH_corr_traits = function(phen,
     
     fixed = rep(FALSE, 1 + ntraits)
     
+=======
+  
+  reduced$post_gen_no_fam <- NA
+  reduced$post_gen_no_fam_se <- NA
+  
+  cov_mat = construct_covmat()
+  
+  for (i in 1:nrow(reduced)) {
+    cur_config = unlist(reduced[i,1:ntraits])
+    
+    lower = rep(-Inf, 1 + ntraits)
+    upper = rep(Inf,  1 + ntraits)
+    
+    lower[-1][!is.na(cur_config) & (cur_config == 1)] <- stats::qnorm(prev_vec[!is.na(cur_config) & (cur_config == 1)], lower.tail = FALSE)
+    upper[-1][!is.na(cur_config) & (cur_config == 0)] <- stats::qnorm(prev_vec[!is.na(cur_config) & (cur_config == 0)], lower.tail = FALSE)
+    
+    fixed = rep(FALSE, 1 + ntraits)
+    
+>>>>>>> parent of 02fc740... Changing and renaming the function estimate_gen_liability_noFH_corr_traits
     # estimate genetic liability & covergence check
     se = NULL 
     vals = list() #store simulated values
     vals.ctr = 1
     while (is.null(se) || se > tol) {
+<<<<<<< HEAD
       gen_liabs = rtmvnorm.gibbs(50e3, 
                                  burn_in = 1000,
                                  sigma = cov_mat,
+=======
+      gen_liabs = rtmvnorm.gibbs(n_sim = 50e3, 
+                                 burn_in = 1000,
+                                 covmat  = cov_mat,
+>>>>>>> parent of 02fc740... Changing and renaming the function estimate_gen_liability_noFH_corr_traits
                                  lower = lower, 
                                  upper = upper,
                                  fixed = fixed)
