b239abfbf9964add93f86c6549c2b35ab53acb58
EmilMiP <emp@profibermail.dk>
2022-09-07 13:22:15 +0200

    function to prepare LTFH++ input and convert from list entry to the longer format.

--- R/prepare_LTFHPlus_input.R
+++ R/prepare_LTFHPlus_input.R
@@ -1,35 +1,5 @@
 utils::globalVariables("cip")
 
-# library(dplyr)
-# library(stringr)
-# library(LTFHPlus)
-# 
-# fam = tibble(fam_id = list(fam_id = c(1, 2, 3)),
-#              pid    = list(pid    = c(list(paste0("pid", 1:4)), list(paste0("pid", 5:10)), list("pid11"))),
-#              role   = list(role   = c(list(c("o", "f", "m", "s1")), list(c("o", "f", "m", "s1", "s2", "s3")), list(c("o"))))) %>%
-#   tidyr::unnest(cols = c(fam_id, pid, role)) %>%
-#   #tidyr::unnest(cols = c(fam_id, pid, role)) %>%
-#   #just adding some info
-#   mutate(sex    = sample(x = 0:1, size = n(), replace = T),
-#          status = sample(x = 0:1, size = n(), replace = T),
-#          current_age    = sample(x = 10:100, size = n(), replace = T),
-#          aoo    = sapply(seq_along(status), function(cur_ind) {
-#            ifelse(status[cur_ind] == 1, sample(10:current_age[cur_ind], size = 1), NA)
-#            }),
-#          age = pmin(current_age, aoo, na.rm = T),
-#          birth_year = 2022 - age) %>%
-#   print()
-# 
-# 
-# CIP = expand.grid(list(age = 1:100,
-#                        birth_year = 1900:2022,
-#                        sex = 0:1)) %>%
-#   group_by(sex, birth_year) %>%
-#   mutate(cip = (1:n() - 1)/n() * .1) %>%
-#   ungroup() %>%
-#   print()
-
-
 #' Attempts to convert the list entry input format to a long format
 #' 
 #' @param family a tibble with two entries, family id and personal id. personal id should end in "_role", if a role column is not present.
@@ -38,14 +8,12 @@ utils::globalVariables("cip")
 #' @param role_col column name that holds the role
 #' 
 #' @return returns a format similar to \code{prepare_LTFHPlus_input}, which is used by \code{estimate_liability}
-#' 
-#' 
+#'
+#' @export 
 
 convert_format = function(family, threshs, personal_id_col = "pid", role_col = NULL) {
   # standardising input -----------------------------------------------------
   
-  
-  print(family)
   #are there any list columns in family (list entry format)?
   which_list_columns = which(sapply(family, is.list))
   if (length(which_list_columns) > 0) { 
@@ -59,10 +27,10 @@ convert_format = function(family, threshs, personal_id_col = "pid", role_col = N
       family[[role_col]] = strsplit(family[[personal_id_col]], "_(?=[^_]+$)", perl=TRUE) %>% sapply(., function(x) x[2])
       family[[personal_id_col]] = strsplit(family[[personal_id_col]], "_(?=[^_]+$)", perl=TRUE) %>% sapply(., function(x) x[1])
       
-      cat("We've tried converting to a long format internally. If you see this print, please run prepare_LTFHPlus_input and use the .tbl input! \n")
+      cat("We've tried converting from list entries to a long format internally. If you see this print, please run prepare_LTFHPlus_input and use the .tbl input going forward! \n")
     } else {
       if (is.null(role_col)) stop("Please provide family roles for each family member. e.g. father(f), mother(m), siblings (s1-s9), etc.") 
-      stop("We weren't able to convert data input automatically. Please use prepare_LTFHPlus_input. \n")
+      stop("We weren't able to convert data input automatically. Please use prepare_LTFHPlus_input and use the .tbl input!\n")
     }
   }
   .tbl = left_join(family, threshs, by = personal_id_col)
@@ -80,6 +48,7 @@ convert_format = function(family, threshs, personal_id_col = "pid", role_col = N
 #' @param personal_id_col Column that contains the personal ID
 #' @param role_col Column that cotnains the role of each individual
 #' 
+#' @export
 
 prepare_LTFHPlus_input = function(family, CIP, 
                                   CIP_columns = c("sex", "birth_year", "age"), 
@@ -104,23 +73,4 @@ prepare_LTFHPlus_input = function(family, CIP,
 
   dplyr::select(family, !!as.symbol(fam_id_col), !!as.symbol(personal_id_col), !!as.symbol(role_col), lower, upper)
 }
-# 
-# input =  prepare_LTFHPlus_input(family = fam, CIP = CIP, role_col = "role", use_fixed_case_thr = F)
-# sim = LTFHPlus::simulate_under_LTM()
-# sim$fam_ID %>% tidyr::unnest()
-# 
-# 
-# fam = input %>% tidyr::unnest(pid, role) %>% mutate(pid = paste0(pid, "_", role)) %>% select(fam_id, pid) %>% tidyr::nest(pid)
-# thr = input %>%  tidyr::unnest(pid) %>% select(-role, -fam_id)# %>% rename(fam_id2 = fam_id, fam_id = pid) %>% rename(pid = fam_id2) %>% select(-1)
-# 
-# .tbl = convert_format(family = fam, threshs = thr, personal_id_col = "pid", role_col = "role")
-# 
-# estimate_liability(family = fam,
-#                    threshs = thr,
-#                    h2 = .5,
-#                    pid = "pid",
-#                    fam_id = "fam_id")
-# estimate_liability(.tbl = .tbl,
-#                    h2 = .5,
-#                    pid = "pid",
-#                    fam_id = "fam_id")
+
