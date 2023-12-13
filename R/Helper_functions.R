82c159d1aadf300ba88f3fb5c27bdd39d62d5186
EmilMiP <emp@profibermail.dk>
2023-12-13 11:14:56 +0100

    added the prepare_graph function

--- R/Helper_functions.R
+++ R/Helper_functions.R
@@ -665,3 +665,124 @@ convert_observed_to_liability_scale <- function(obs_h2 = 0.5, pop_prev = 0.05, p
   }
 }
 
+
+# graph helper functions --------------------------------------------------
+
+#' 
+#' Add pseudo point to graph
+#' 
+#' Internal function. Identifies the proband and makes a duplicated point that will act as genetic liability in kinship matrix
+#' 
+#' @param fam_graph family graph center on proband
+#' @param index_id id of proband.
+#' 
+#' @return returns family graph with a pseudo point added that will have the same connections as the proband point. For kinship construction, the relationship to the proband must be adjusted to 1 \* h2 (and not 0.5 \* h2).
+
+add_gen_liab_to_graph = function(fam_graph, index_id) {
+  # find all index edges
+  index_edges = stringr::str_subset(igraph::as_ids(igraph::E(fam_graph)), index_id)
+  # subset edges to and from index with new id - here with "_g" added
+  
+  # we need to handle the no edges case, i.e. one point graphs.
+  if (length(index_edges)  > 0) {
+    gen_liab_edges = stringr::str_replace(index_edges, index_id, paste0(index_id, "_g")) %>% 
+      # format into vector with start and end of edge at positions 1 & 2, 3 & 4, etc
+      # as defined by igraph.
+      stringr::str_split("\\|") %>% 
+      # rbind is faster than cbind
+      do.call("rbind", .) %>%
+      # transpose for 2xN
+      t() %>% 
+      # c for column wise conversion to vector - each edge is a column
+      c()
+  }
+  
+  fam_graph %>% 
+    igraph::add.vertices(., nv = 1, name = paste0(index_id, "_g")) %>% 
+    # adding edges found above and edge between _g and index_id
+    igraph::add.edges(., edges = c(if (length(index_edges) > 0) gen_liab_edges,
+                                   paste0(rep(index_id, 4), c("", "_g", "_g", ""))))
+}
+
+
+#' Construct kinship matrix from graph
+#' 
+#' construct the kinship matrix from a graph representation of a family, centered on an index person (proband). 
+#' 
+#' 
+#' @param fam_graph graph.
+#' @param h2 heritability.
+#' @param index_id proband id. Only used in conjuction with add_ind = TRUE.
+#' @param add_ind add genetic liability to the kinship matrix. Defaults to true.
+#' 
+#' @return A kinship matrix. 
+#' 
+#' @export
+#' 
+
+get_kinship = function(fam_graph, h2, index_id = NA, add_ind = TRUE) {
+  if (add_ind) {
+    index_id_g = paste0(index_id, "_g")
+    # adding in point to act as genetic liability for index person
+    # need to adjust individual and genetic liab point's internal relationship later
+    fam_graph = add_gen_liab_to_graph(fam_graph, index_id)
+    
+  }
+  
+  # in-distances
+  ph = igraph::distances(fam_graph, mode = "in")
+  rnames = rownames(ph)
+  nrows = nrow(ph)
+  
+  # construct dummy matrix
+  new_distances = matrix(NA, ncol = nrows, nrow = nrows)
+  rownames(new_distances) = rnames
+  colnames(new_distances) = rnames
+  
+  # fill dummy matrix
+  for (i in 1:(nrows - 1)) {
+    for (j in (i + 1):nrows) {
+      new_distances[i,j] <- new_distances[j,i] <- min(ph[i, ] + ph[j, ]) 
+    }
+  }
+  
+  # calculating kinship matrix, _g needs adjustment
+  kinship = 0.5^new_distances
+  if (add_ind) {
+    # multipying with h2 and fixing diagonal to 1 - except for gen liab
+    kinship[index_id, index_id_g] <- kinship[index_id_g, index_id] <- kinship[index_id_g, index_id_g] <- 1
+    kinship = kinship * h2
+    diag(kinship)[-which(rownames(kinship) == index_id_g)] <- 1
+    
+  } else {
+    kinship = kinship * h2
+    diag(kinship) <- 1
+  }
+  
+  return(kinship)
+}
+
+
+#' 
+#' construct all combinations of input vector
+#' 
+#' pastes together all combinations of input vector
+#' 
+#' @param vec vector of strings
+#' 
+#' @return A vector of strings is returned.
+#' 
+#' @examples
+#' \dontrun{
+#' get_all_combs(letters[1:3])
+#' }
+#' 
+#' 
+get_all_combs = function(vec) {
+  # outer: creates all combinations of entries in vec
+  # then we select only the off diagonal entries
+  outer(vec, vec, paste, sep = "_")[!diag(TRUE, nrow = length(vec))]
+  # results in length(vec) * (length(vec) - 1) entries
+  # since it is the number of entries in the upper *and* lower triangle
+  # number of entries in either is given by length(vec) * (length(vec) - 1)/2
+}
--- R/prepare_LTFHPlus_input.R
+++ R/prepare_LTFHPlus_input.R
@@ -1,4 +1,13 @@
 utils::globalVariables("cip")
+utils::globalVariables("all_combinations")
+utils::globalVariables("children")
+utils::globalVariables("comb")
+utils::globalVariables("distances")
+utils::globalVariables("from")
+utils::globalVariables("nchildren")
+utils::globalVariables("parent_id")
+utils::globalVariables("ph")
+utils::globalVariables("to")
 
 #' Attempts to convert the list entry input format to a long format
 #' 
@@ -72,5 +81,100 @@ prepare_LTFHPlus_input = function(family, CIP,
 # returning formatted input -----------------------------------------------
 
   dplyr::select(family, !!as.symbol(fam_id_col), !!as.symbol(personal_id_col), !!as.symbol(role_col), lower, upper)
+}
+
+
+
+#' Construct graph from register information
+#' 
+#' \code{prepare_graph} constructs a graph based on mother, father, and offspring links. 
+#'
+#' @param .tbl tibble with columns icol, fcol, mcol. Additional columns will be attributes in the constructed graph.
+#' @param icol column name of column with proband ids.
+#' @param fcol column name of column with father ids.
+#' @param mcol column name of column with mother ids.
+#' @param missingID_patterns string of missing values in the ID columns. Multiple values can be used, but must be separated by "|". Defaults to "0".
+#' 
+#' @return An igraph object. A (directed) graph object based on the links provided in .tbl. 
+#' 
+#' @export
+#' @importFrom dplyr %>% rename relocate mutate filter group_by summarise select bind_rows pull
+
+
+prepare_graph = function(.tbl, fcol, mcol, icol, missingID_patterns = "^0$") {
+  
+  # formatting .tbl from trio info to graph compatible input
+  prep = .tbl %>% 
+    # making from column
+    tidyr::pivot_longer(cols = c(!!as.symbol(fcol), !!as.symbol(mcol)), 
+                        values_to = "from") %>% 
+    # renaming id to "to"
+    rename(to = !!as.symbol(icol)) %>% 
+    # reloacting to and from columns to first two columns
+    relocate(from, to) %>% # directed graph -> order is important! 
+    # replacing "0"s with NA to ease later computation and reflect true data set
+    # with missing / unknown links
+    mutate(to = ifelse(str_detect(to, missingID_patterns), NA, to),
+           from = ifelse(str_detect(from, missingID_patterns), NA, from)) 
+  
+  # remove connections with unknown links, i.e. only known links / edges
+  parent_links = prep %>% 
+    filter(!is.na(to), !is.na(from)) %>% 
+    # ensure from and to are character vectors
+    mutate(from = as.character(from),
+           to = as.character(to))
+  
+  # we need to add a direct link between (full) siblings
+  sibling_links = .tbl %>% 
+    filter(str_detect(!!as.symbol(fcol), missingID_patterns, negate = TRUE), 
+           str_detect(!!as.symbol(mcol), missingID_patterns, negate = TRUE)) %>% 
+    mutate(parent_id = purrr::map2_chr(
+      .x = !!as.symbol(fcol),
+      .y = !!as.symbol(mcol),
+      ~ paste0(sort(c(.x, .y)), collapse = "_")
+    )) %>% 
+    group_by(parent_id) %>% 
+    summarise(children = list(!!as.symbol(icol))) %>% 
+    mutate(nchildren = sapply(children, length)) %>% 
+    filter(nchildren > 1) %>% 
+    mutate(all_combinations = purrr::map(.x = children, ~ get_all_combs(.x))) %>% 
+    # avoiding many duplicate rows by selecting only columns to unnest
+    select(all_combinations) %>% 
+    tidyr::unnest(cols = c(all_combinations)) %>% 
+    mutate(ph = str_split(all_combinations, "_"),
+           from = sapply(ph, function(x) x[1]),
+           to = sapply(ph, function(x) x[2])) %>% 
+    select(from, to)
+  
+  # combining sibling and parent links
+  if (nrow(sibling_links) > 0) {
+    graph_input = bind_rows(parent_links, sibling_links)
+  } else {
+    graph_input = parent_links
+  }
+  
+  
+  graph = igraph::graph_from_data_frame(d = graph_input)
+  
+  # isolating potential solo nodes: where to or from column is NA
+  # extracting only node names
+  solo = prep %>% 
+    mutate(comb = purrr::map2_chr(.x = from, 
+                                  .y = to,
+                                  ~ paste0(sort(c(.x, .y)), collapse = "_"))) %>% 
+    filter(str_detect(comb, "_", negate = TRUE)) %>% 
+    pull(comb)
+  
+  # all linked nodes; no NAs in to or from,
+  # just a list of node names
+  duos = graph_input %>% 
+    filter(!is.na(from) & !is.na(to)) %>% 
+    select(from, to) %>% unlist() %>% unique()
+  
+  # which nodes appear only in solo nodes, but not in duo nodes?
+  # meaning they have no links
+  solo_points = setdiff(solo, as.character(duos))
+  
+  igraph::add.vertices(graph, nv = length(solo_points), name = solo_points) 
 }
 
